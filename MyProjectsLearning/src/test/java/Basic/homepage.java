a08d840df470918725b1f9e7099db358f0689496
kusha <kusha@KUSHPC>
2026-03-06 15:23:27 +0530

    This is a sample code for home page.

---
+++ MyProjectsLearning/src/test/java/Basic/homepage.java
@@ -0,0 +1,12 @@
+package Basic;
+
+public class homepage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello this is the homepage");
+
+	}
+
+}
